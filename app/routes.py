b4232ec01907af6965c4d7f475b39a97fa61a5e3
Nathan Stirling <2254880S@student.gla.ac.uk>
2021-02-15 15:26:37 +0000

    added image page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
+@app.route('/index')
+
+
+@app.route('/images')
+def images():
+    return render_template('images.html')
+
 @app.route('/login')
 def index():
     return render_template('login.html')
