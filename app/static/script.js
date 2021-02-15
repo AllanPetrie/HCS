414a07ad077f6def2c23bcd4fcbfdd514eeab015
Nathan Stirling <2254880S@student.gla.ac.uk>
2021-02-15 17:20:31 +0000

    end of day from nathan

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,16 @@
-function validate(){
+function validate(id){
+    img_src = document.getElementById(id).src;
     window.location.assign("imageselect")
+    changeImage(img_src)
+  
 }
+
+function changeImage(src){
+    console.log("In next function")
+    image = document.getElementById("image2")
+    img.src = src
+}
+
+// function change_image(src){
+
+// }
