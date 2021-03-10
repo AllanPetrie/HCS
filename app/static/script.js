4f54852f10841f1c557a55f26d928935302141bd
AllanPetrie <2254840P@student.gla.ac.uk>
2021-03-10 10:33:26 +0000

    Updating image selection to store whether selected image is valid.

--- app/static/script.js
+++ app/static/script.js
@@ -1,25 +1,33 @@
 function validate(id){
     
     img_src = document.getElementById(id).src;
-    img_src = img_src.slice(-25)
+    img_src = img_src.slice(-25);
 
-    new_src = "..".concat(img_src)
+    new_src = "..".concat(img_src);
 
-    sessionStorage.setItem("image", new_src)
+    actual_image = sessionStorage.getItem("image", new_src);
     // window.open(img_src);
     // window.open(img_src),'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
-    window.location.assign("imageselect")
+    
+    if (new_src == actual_image) {
+        sessionStorage.setItem("correctImage", true); 
+    } else {
+        sessionStorage.setItem("correctImage", false);
+    }
+
+    window.location.assign("imageselect");
+
     
   
 }
 
 function changeImage(){
-    console.log("In next function")
+    console.log("In next function");
     
-    src = sessionStorage.getItem("image")
-    console.log(src)
-    image = document.getElementById("image2")
-    image.src = src
+    src = sessionStorage.getItem("image");
+    console.log(src);
+    image = document.getElementById("image2");
+    image.src = src;
 }
 
 // function change_image(src){
